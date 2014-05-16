ab16fcf87d59896fbf3c467ed24add2d3f9aee69
Emmanuel Lepage Vallee <elv1313@gmail.com>
2014-05-15 23:27:31 -0400

    Move to a more object oriented architechture

--- init.lua
+++ init.lua
@@ -15,38 +15,6 @@ local core = require     'lgi.core'
 local GLib = lgi.require 'GLib'
 local type,unpack = type,unpack
 
--- This example come from the official DBus spec
-local xml = [=[ <!DOCTYPE node PUBLIC '-//freedesktop//DTD D-BUS Object Introspection 1.0//EN'
-  'http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd'>
-<node name='/com/example/sample_object'>
-  <interface name='com.example.SampleInterface'>
-    <method name='Frobate'>
-      <arg name='foo' type='i' direction='in'/>
-      <arg name='bar' type='s' direction='out'/>
-      <arg name='baz' type='a{us}' direction='out'/>
-      <!--<annotation name='org.freedesktop.DBus.Deprecated' value='true'/>'-->
-    </method>
-    <method name='Bazify'>
-      <arg name='bar' type='(iiu)' direction='in'/>
-      <arg name='bar' type='v' direction='out'/>
-    </method>
-    <method name='Barify'>
-      <arg name='bar' type='a{ss}' direction='in'/>
-      <arg name='foo' type='i' direction='in'/>
-      <arg name='bar' type='i' direction='out'/>
-    </method>
-    <method name='Mogrify'>
-      <arg name='bar' type='(iiav)' direction='in'/>
-    </method>
-    <signal name='Changed'>
-      <arg name='new_value' type='b'/>
-    </signal>
-    <property name='Bar' type='s' access='readwrite'/>
-  </interface>
-  <node name='child_of_sample_object'/>
-  <node name='another_child_of_sample_object'/>
-</node>]=]
-
 
 --------------
 --  OBJECT  --
@@ -99,13 +67,11 @@ local property_set = {
 }
 
 
-local introspection_data = nil
-
 -- Get the interface_info from its name
 local ifaces = {}
-local function iface_lookup(name)
+local function iface_lookup(service,name)
     if not ifaces[name] then
-        local iface = introspection_data:lookup_interface('com.example.SampleInterface')
+        local iface = service.introspection_data:lookup_interface(service.iname)
         ifaces[name] = iface
         return iface
     end
@@ -114,14 +80,14 @@ end
 
 -- Get a method information
 
-local function common_get_info(cache,interface_name,method,func)
+local function common_get_info(service,cache,interface_name,method,func)
     if not cache[interface_name] then
         cache[interface_name] = {}
     end
     local iface_sigs = cache[interface_name]
 
     if not iface_sigs[method] then
-        local iface = iface_lookup(interface_name)
+        local iface = service:iface_lookup(interface_name)
         if iface then
             local info = iface[func](iface,method)
             if info then
@@ -133,26 +99,26 @@ local function common_get_info(cache,interface_name,method,func)
     return iface_sigs[method]
 end
 
-local function get_property_info(interface_name,property)
+local function get_property_info(service,interface_name,property)
     local prop_info = {}
-    return common_get_info(prop_info,interface_name,property,"lookup_property")
+    return service:common_get_info(prop_info,interface_name,property,"lookup_property")
 end
 
-local function get_method_info(interface_name,method)
+local function get_method_info(service,interface_name,method)
     local method_info = {}
-    return common_get_info(method_info,interface_name,method,"lookup_method")
+    return service:common_get_info(method_info,interface_name,method,"lookup_method")
 end
 
-local function get_signal_info(interface_name,signal)
+local function get_signal_info(service,interface_name,signal)
     local sign_info = {}
-    return common_get_info(sign_info,interface_name,signal,"lookup_signal")
+    return service:common_get_info(sign_info,interface_name,signal,"lookup_signal")
 end
 
 
 
 -- Get a method output signature
-local function get_out_signature(interface_name,method)
-    local info = get_method_info(interface_name,method)
+local function get_out_signature(service,interface_name,method)
+    local info = service:get_method_info(interface_name,method)
     if info then
         local ret_t = "("
         for k,v in pairs(info.out_args or {}) do
@@ -164,90 +130,171 @@ local function get_out_signature(interface_name,method)
     return ""
 end
 
---------------
--- CLOSURES --
---------------
+-----------------------
+--  Service methods  --
+-----------------------
 
-
--- Called when a remote method is called
--- This closure dispatch the calls to the right function
-local method_call_guard, method_call_addr = core.marshal.callback(Gio.DBusInterfaceMethodCallFunc ,
-  function(conn, sender, path, interface_name,method_name,parameters,invok)
-  if methods[method_name] then
-    local rets = {methods[method_name](unpack(parameters.value))}
-    local out_sig = get_out_signature(interface_name,method_name)
-
-    local gvar = GLib.Variant(out_sig,rets)
-    Gio.DBusMethodInvocation.return_value(invok,gvar)
-  end
-end)
-
--- Called when there is a property request (get the current value)
-local property_get_guard, property_get_addr = core.marshal.callback(Gio.DBusInterfaceGetPropertyFunc , 
-  function(conn, sender, path, interface_name,property_name,parameters,error)
-    local sig = get_property_info(interface_name,property_name).signature
-    if property_get[property_name] then
-        return GLib.Variant(sig,property_get[property_name]())
+local function register_object(service,name)
+    local function on_conn_aquired(conn,name)
+        
     end
-    return GLib.Variant(sig)
-end)
-
--- Called when there is a property request (set the current value)
-local property_set_guard, property_set_addr = core.marshal.callback(Gio.DBusInterfaceSetPropertyFunc , 
-  function(conn, sender, path, interface_name,method_name,parameters)
-  print("Set a property")
-end)
+end
 
 
--- Called when the bus is aquired, it is used to register the
--- XML spec
-local bus_aquired = lgi.GObject.Closure(function(conn, name)
-  introspection_data = Gio.DBusNodeInfo.new_for_xml(xml)
-  print("The bus is aquired!")
-  local iface_info = iface_lookup('com.example.SampleInterface')
+--------------------
+--  Module gears  --
+--------------------
 
-  --introspection_data
-  local registration_id,vat = conn:register_object (
-    "/com/example/SampleInterface/Test",
-    iface_info,
-    Gio.DBusInterfaceVTable({
-      method_call   = method_call_addr ,
-      get_property  = property_get_addr,
-      set_property  = property_get_addr,
-    }),
-    {},  --/* user_data */
-    lgi.GObject.Closure(function()
-      print("Closing the object")
-    end),  --/* user_data_free_func */
-    lgi.GObject.Closure(function()
-      print("There was an error")
+local module = {}
+
+-- Create a new service
+function module.create_service(iname,xml_introspection)
+
+    --Setup object
+    local service = {properties = {},iname = iname,introspection_data=nil,
+        get_out_signature = get_out_signature,
+        get_property_info = get_property_info,
+        get_method_info   = get_method_info,
+        common_get_info   = common_get_info,
+        iface_lookup      = iface_lookup,
+    }
+
+    --------------
+    -- CLOSURES --
+    --------------
+
+
+    -- Called when a remote method is called
+    -- This closure dispatch the calls to the right function
+    local method_call_guard, method_call_addr = core.marshal.callback(Gio.DBusInterfaceMethodCallFunc ,
+    function(conn, sender, path, interface_name,method_name,parameters,invok)
+        if methods[method_name] then
+            local rets = {methods[method_name](unpack(parameters.value))}
+            local out_sig = service:get_out_signature(interface_name,method_name)
+
+            local gvar = GLib.Variant(out_sig,rets)
+            Gio.DBusMethodInvocation.return_value(invok,gvar)
+        end
     end)
-  )
-end)
 
--- Called when the name is aquired
-local name_aquired = lgi.GObject.Closure(function(conn, name)
-  print("The name is aquired!")
-end)
+    -- Called when there is a property request (get the current value)
+    local property_get_guard, property_get_addr = core.marshal.callback(Gio.DBusInterfaceGetPropertyFunc , 
+    function(conn, sender, path, interface_name,property_name,parameters,error)
+        local sig = service:get_property_info(interface_name,property_name).signature
+        if property_get[property_name] then
+            return GLib.Variant(sig,property_get[property_name]())
+        end
+        return GLib.Variant(sig)
+    end)
 
--- Called when the name is lost
-local name_lost = lgi.GObject.Closure(function(conn, name)
-  print("The name is lost!")
-end)
+    -- Called when there is a property request (set the current value)
+    local property_set_guard, property_set_addr = core.marshal.callback(Gio.DBusInterfaceSetPropertyFunc , 
+    function(conn, sender, path, interface_name,method_name,parameters)
+        print("Set a property")
+    end)
+
+    print("attempting to create a server")
+    -- Called when the bus is aquired, it is used to register the
+    -- XML spec
+    local bus_aquired = lgi.GObject.Closure(function(conn, name)
+        service.connection = conn
+        if service.on_connection_aquired then
+            service.on_connection_aquired(conn,name)
+            service.on_connection_aquired = nil
+        end
+        service.introspection_data = Gio.DBusNodeInfo.new_for_xml(xml_introspection)
+        print("The bus is aquired!")
+        local iface_info = iface_lookup(service,iname)
+
+        --introspection_data
+        local registration_id,vat = conn:register_object (
+            "/com/example/SampleInterface/Test",
+            iface_info,
+            Gio.DBusInterfaceVTable({
+            method_call   = method_call_addr ,
+            get_property  = property_get_addr,
+            set_property  = property_get_addr,
+            }),
+            {},  --/* user_data */
+            lgi.GObject.Closure(function()
+            print("Closing the object")
+            end),  --/* user_data_free_func */
+            lgi.GObject.Closure(function()
+            print("There was an error")
+            end)
+        )
+    end)
+
+    -- Called when the name is aquired
+    local name_aquired = lgi.GObject.Closure(function(conn, name,c,d,e)
+        print("The name is aquired!",c,d,e)
+    end)
+
+    -- Called when the name is lost
+    local name_lost = lgi.GObject.Closure(function(conn, name)
+        print("The name is lost!")
+    end)
 
 
--- First, aquire the Session bus
-local owner_id = Gio.bus_own_name(Gio.BusType.SESSION,
-  "com.example.SampleInterface",           --Interface name
-  Gio.BusNameOwnerFlags.ALLOW_REPLACEMENT, --We want to take control of the existing service
-  bus_aquired,                             --Called when the bus is aquired
-  name_aquired,                            -- Called when the name is aquired
-  name_lost                                -- Called when the name is lost
-  --Errors handling is not implemented
-)
+    -- First, aquire the Session bus
+    local owner_id = Gio.bus_own_name(Gio.BusType.SESSION,
+    iname,                                   --Interface name
+    Gio.BusNameOwnerFlags.ALLOW_REPLACEMENT, --We want to take control of the existing service
+    bus_aquired,                             --Called when the bus is aquired
+    name_aquired,                            -- Called when the name is aquired
+    name_lost                                -- Called when the name is lost
+    --Errors handling is not implemented
+    )
+end
+
+
+
+
+
+
+
+
+-- Test
+local service = module.create_service("com.example.SampleInterface",[=[ <!DOCTYPE node PUBLIC '-//freedesktop//DTD D-BUS Object Introspection 1.0//EN'
+  'http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd'>
+<node name='/com/example/sample_object'>
+  <interface name='com.example.SampleInterface'>
+    <method name='Frobate'>
+      <arg name='foo' type='i' direction='in'/>
+      <arg name='bar' type='s' direction='out'/>
+      <arg name='baz' type='a{us}' direction='out'/>
+      <!--<annotation name='org.freedesktop.DBus.Deprecated' value='true'/>'-->
+    </method>
+    <method name='Bazify'>
+      <arg name='bar' type='(iiu)' direction='in'/>
+      <arg name='bar' type='v' direction='out'/>
+    </method>
+    <method name='Barify'>
+      <arg name='bar' type='a{ss}' direction='in'/>
+      <arg name='foo' type='i' direction='in'/>
+      <arg name='bar' type='i' direction='out'/>
+    </method>
+    <method name='Mogrify'>
+      <arg name='bar' type='(iiav)' direction='in'/>
+    </method>
+    <signal name='Changed'>
+      <arg name='new_value' type='b'/>
+    </signal>
+    <property name='Bar' type='s' access='readwrite'/>
+  </interface>
+  <node name='child_of_sample_object'/>
+  <node name='another_child_of_sample_object'/>
+</node>]=])
+
+
+
+
+
+
+--TODO check if a mainloop and running or start one
 
 -- This is a test app, so we start the loop directly
-print(lgi.GObject.object_ref,error)
 local main_loop = GLib.MainLoop()
-main_loop.run(main_loop)
+main_loop:run()
 
+return module
