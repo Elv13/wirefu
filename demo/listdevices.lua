e421f25e05ed295830239cabf8e178578eb596c1
AxelTB <axel99999@hotmail.com>
2014-11-20 22:48:14 +0100

    Lookup module for asynchronus service search

--- demo/listdevices.lua
+++ demo/listdevices.lua
@@ -1,10 +1,25 @@
 local lgi  = require     'lgi'
-local wirefu = require("wirefu")
 local GLib = lgi.require 'GLib'
 
-wirefu.SYSTEM.a.b.c.d().get(function (work)
-    print("It worked:",work)
-end)
+local wirefuLookup = require('wirefu.lookup')
+
+-- Demo of wirefu/lookup module
+-- USAGE: wirefu/demo/listdevices.lua [pattern]
+
+
+-- Create lookup instance
+wirefuLookup()
+
+-- Print function
+local printNames = function (nameList)
+        for i=1,#nameList do
+                print(nameList[i])
+        end
+        os.exit()
+end
+
+
+wirefuLookup.getServices(printNames,arg[1])
 
 local main_loop = GLib.MainLoop()
 main_loop:run()
