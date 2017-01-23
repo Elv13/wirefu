eb0c1d6d5fa5346e5318d9e0ea6c4725a4f4bdfe
Emmanuel Lepage Vallee <elv1313@gmail.com>
2017-01-23 11:52:14 -0500

    demo: Add UPower signal demo

--- demo/upower.lua
+++ demo/upower.lua
@@ -5,6 +5,11 @@ local GLib = lgi.require 'GLib'
 wirefu.SYSTEM.org.freedesktop.UPower("/org/freedesktop/UPower").HibernateAllowed():get(function (work)
     print("It worked:",work)
 end)
+
+wirefu.SYSTEM.org.freedesktop.UPower("/org/freedesktop/UPower").Changed:connect(function (work)
+    print("STATUS CHANGED")
+end)
+
 print("async")
 
 local main_loop = GLib.MainLoop()
